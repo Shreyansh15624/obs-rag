7355dd6c6ba14d3b1e980be4591f230ec16f147f
Shreyansh15624 <shreyanshvajroju@gmail.com>
2026-01-13 10:20:39 +0000

    Refactored the main.py according to the documentation & best practices around speed & efficiency

--- main.py
+++ main.py
@@ -3,187 +3,72 @@ import sys
 from dotenv import load_dotenv
 
 # New Imports
-from langchain_community.document_loaders import ObsidianLoader
-from angchain_text_spliters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnablePassThrough
 
-
-# Imports to be removed
-from google import genai
-from google.genai import types
-
-# Local Function Imports, the files are subject to potential changes in functioning
-from functions.get_files_info import get_files_info, schema_get_files_info
-from functions.get_file_content import get_file_content, schema_get_file_content
-from functions.write_file import write_file, schema_write_file
-from functions.run_python_file import run_python_file, schema_run_python_file
 # New function addition
-from functions.obsidian_searcher import search_notes
+from functions import search_notes
+
 from enum import Enum
 
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY") # Code Accesses the API Key
-client = genai.Client(api_key=api_key) # Application connects with the Model remotely
 
-# Hypnotizing AI for safety 👁️👄👁️ -> 😵‍💫 -> 🫡
-system_prompt = """
-You are a helpful AI coding agent.
+def main():
+    # Settign up the model, best suitable for speed!
+    if not os.getenv("GOOGLE_API_KEY"):
+        print("Error: GOOGLE_API_KEY not found in .env")
+        return
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3) 
 
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    # Hypnotizing AI for best performance 👁️👄👁️ -> 😵‍💫 -> ⚡😎⚡
+    system_prompt = f"""
+You are an intelligent "Second Brain" AI Assistant Agent.
+You have access to the user's personal notes.
 
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
+Here is the context retrieved from the notes.
+{context}
 
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
+Question: {question}
 
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_write_file,
-        schema_run_python_file,
-    ]
-)
-
-
-available_functions_dict = {
-    "get_files_info" : get_files_info,
-    "get_file_content" : get_file_content,
-    "write_file" : write_file,
-    "run_python_file" : run_python_file
-}
-
-# For the Switch case, to simplify code understanding & avoiding if-elif-else ladders
-available_functions_enum = Enum("available_functions_enum", ["get_files_info", "get_file_content", "write_file", "run_python_file"])
-
-global arguments # Because we are using in isolated functions too
-arguments = sys.argv
-if len(arguments) < 2:
-    print("Prompt is empty!")
-    sys.exit(1)
-
-
-def call_function(function_call_part, working_directory, verbose=False):
-    # This function is for calling the functions that allows our AI model to make changes to
-    # the user project, in this case the Provided Example: "calculator/" App 
-    try:
-        if verbose:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        else:
-            print(f" - Calling function: {function_call_part.name}")
-        match function_call_part.name:
-            case available_functions_enum.write_file.name:
-                arg_dict_2 = {
-                    "working_directory" : working_directory,
-                    "file_path" : function_call_part.args["file_path"],
-                    "content" : function_call_part.args["content"]
-                }
-                function_result = available_functions_dict[function_call_part.name](**arg_dict_2)
-            case available_functions_enum.get_file_content.name:
-                arg_dict_2 = {
-                    "working_directory" : working_directory,
-                    "file_path" : function_call_part.args["file_path"]
-                }
-                function_result = available_functions_dict[function_call_part.name](**arg_dict_2)
-            case available_functions_enum.get_files_info.name:
-                arg_dict_2 = {
-                    "working_directory" : working_directory,
-                    "directory" : (function_call_part.args["directory"] if "directory" in function_call_part.args else ".")
-                }
-                function_result = available_functions_dict[function_call_part.name](**arg_dict_2)
-            case available_functions_enum.run_python_file.name:
-                arg_dict_2 = {
-                    "working_directory" : working_directory,
-                    "file_path": function_call_part.args["file_path"]
-                }
-                function_result = available_functions_dict[function_call_part.name](**arg_dict_2)
-        if not function_call_part.name:
-            function_call_result = types.Content(
-                role="tool",
-                parts=[
-                    types.Part.from_function_response(
-                        name=available_functions_dict[function_call_part.name],
-                        response={"error": f"Unknown function: {function_call_part.name}"},
-                    )
-                ],
-            )
-            return function_call_result.parts[0].function_response.response["result"]
-        else:
-            function_call_result = types.Content(
-                role="tool",
-                parts=[
-                    types.Part.from_function_response(
-                        name=function_call_part.name,
-                        response={"result":function_result},
-                    )
-                ],
-            )
-            return function_call_result.parts[0].function_response.response["result"]
-    except Exception as e:
-        return f'Error: Executing Function: {e}'
-
-
-user_prompt = arguments[1]
-
-
-messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]), ]
-for message in messages:
-    print(f"message:\n{messages}\n")
-for i in range(20):
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
-        ),
-    )
-    verbose = False
-    if '--verbose' in arguments:
-        verbose = True
-        print("------------------------------------------------------------------")
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    for candidate in response.candidates:
-        messages.append(candidate.content)
-    function_call_responses = []
-    # 'function_call_responses' can be empty because the data from the previous run will be 
-    # present in the Neural Network of AI remotely. Its similar to 'messages' & will pbe part 
-    # of it after packaging, but messages has a default data added. While 'messages' is the
-    # user input to the model, function_call_responses consists the output acquired by executing
-    # the provided functions, in the 'functions/' directory
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-            arg_dict = {
-                "function_call_part" : function_call_part,
-                # Strictly limits the AI's access to the sub-directory, the real project you want to work with!
-                # If not for this, the AI-Model will gain unrestricted access your device Compromising Personal Information. 
-                "working_directory" : "calculator", # DO NOT CHANGE!!
-                "verbose" : verbose,
-            }
-            # print("\n")
-            function_call_result = call_function(**arg_dict)
-            print(function_call_result)
-            call_responses = types.Part.from_function_response(
-                name=function_call_part.name,
-                response={"result": function_call_result}
-            )
-            function_call_responses.append(call_responses)
-        packaged_function_call_result = types.Content(
-            role="user",
-            parts=function_call_responses
-        ) # This sorts the data for the AI-Model's best understanding
-        messages.append(packaged_function_call_result)
-    else:
-        if response.text:
-            print(f"\nModel's response.text:\n{response.text}")
+Instructions:
+- Answer the question using ONLY the context provided above.
+- If the context doesn't contain the answer, admit that you do not know based on the notes.
+- Cite the source (filename) if available in the context.
+    """
+    # 'context' & 'question' variables will make appearance in the later part of the code!
+    
+    # Feeding the input_prompt placed into the system_prompt to the Model!
+    prompt = ChatPromptTemplate.from_template(system_prompt)
+    
+    # Establishing the Chat Loop
+    print("\n🧠Obsidian RAG Agent Ready! (Type 'exit' to quit)\n")
+    
+    while True:
+        user_query = input("\nYou: ")
+        if user_query.lower() in ("exit", "quit"):
             break
+        
+        print("🔍Searching in yout notes...")
+        
+        # Step-1: Gather the relevant notes from the Vault
+        retieved_context = search_notes(user_query)
+        
+        # Step-2: Generating the Answer
+        prompt_chain = (
+            {"context": lambda x: retieved_context, "question": lambda x: user_query}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+        
+        # Step-3: Returning the response, with a Cool-factor
+        print("🤖AI: ", end=" ")
+        for chunk in prompt_chain.stream({}):
+            print(chunk, end=" ", flush=True)
+        print("\n")
 
-sys.exit(0)
+if __name__=="__main__":
+    main()
